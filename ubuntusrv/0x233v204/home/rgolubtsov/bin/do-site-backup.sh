497263e4af180ee12d2f4027883aede5639ddf30
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2026-01-30 21:00:00 +0300

    Bump copyright year.

--- ubuntusrv/0x233v204/home/rgolubtsov/bin/do-site-backup.sh
+++ ubuntusrv/0x233v204/home/rgolubtsov/bin/do-site-backup.sh
@@ -5,7 +5,7 @@
 # along with its corresponding database, and uploads those tarballs
 # to the FTP server.
 # =============================================================================
-# Written by Radislav (Radicchio) Golubtsov, 2021-2025
+# Written by Radislav (Radicchio) Golubtsov, 2021-2026
 #
 
 # Helper constants.
